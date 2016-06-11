a7062b9f403fee6ef17f64b730c6cb4592d586c4
imamhabib <imamhabibpamungkas@gmail.com>
2016-06-11 11:39:55 +0700

    set url remember token

--- app/views/pages/analisis.blade.php
+++ app/views/pages/analisis.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.default')
 
 @section('content')
-<div class="panel" height="1000px">
-        		<iframe src="http://127.0.0.1:3804" width= "100%" height="100%">
+<div class="panel" style=" height: 1200px">
+        		<iframe src="http://127.0.0.1:6988?{{Auth::user()->remember_token;}}" width= "100%" height="100%">
 
 				</iframe>
 </div>
