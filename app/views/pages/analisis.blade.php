cfa6aee3a5196cb5c55d5744195731e0064ca993
mandes95 <mandes95@gmail.com>
2016-08-22 23:31:46 +0700

    Update analisis.blade.php

--- app/views/pages/analisis.blade.php
+++ app/views/pages/analisis.blade.php
@@ -1,9 +1,9 @@
-@extends('layouts.default')
+@extends('layouts.default2')
 
 @section('content')
 <div class="panel" style=" height: 1200px;" >
 	@if(Auth::user())
-        		<iframe src="http://127.0.0.1:6962?{{Auth::user()->remember_token;}}" width= "100%" height="100%">
+        		<iframe src="http://127.0.0.1:6962?token={{Auth::user()->remember_token;}}" width= "100%" height="100%">
 
 				</iframe>
 	@else
